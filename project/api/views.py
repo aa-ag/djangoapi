d12c414bfb14d70ab5d90a57ce471287968fbd96
aa-ag <aaron.aguerrevere@gmail.com>
2021-07-25 21:27:34 -0500

    added views.py's logic/classes in api

--- project/api/views.py
+++ project/api/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from .serializers import UserSerializer, GroupSerializer
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+     users' endpoint (read & update)
+    """
+    query_set = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+     groups' endpoint (read & update)
+    """
+    query_set = Group.objects.all()
+    serializer_class = GroupSerializer
